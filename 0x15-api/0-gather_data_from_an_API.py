2b05c69251883522a889d293d0e2128ea5654c67
Anthony <huggins9000211@gmail.com>
2020-05-04 14:19:15 -0400

    test

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+""" test """
+if __name__ == "__main__":
+    import requests
+    import sys
+    import json
+    totalCount = 0
+    completedCount = 0
+    x = requests.get('https://jsonplaceholder.typicode.com/todos')
+    nameReq = requests.get('https://jsonplaceholder.typicode.com/users/{}'.format(sys.argv[1]))
+    empName = json.loads(nameReq.text)['name']
+    compledTasks = []
+    myList = []
+    for y in json.loads(x.text):
+        if str(y['userId']) == sys.argv[1]:
+            myList.append(y)
+            if y['completed']:
+                completedCount += 1
+                compledTasks.append(y['title'])
+            totalCount += 1
+    print("Employee {} is done with tasks({}/{}):".format(empName, completedCount, totalCount))
+    for y in compledTasks:
+        print("\t {}".format(y))
