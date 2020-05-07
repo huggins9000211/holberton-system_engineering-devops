b41b2a2e17b7d21111bbb24875f82aaab75221f3
Anthony <huggins9000211@gmail.com>
2020-05-07 15:32:44 -0400

    test

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+""" test """
+import json
+import requests
+import sys
+
+
+def number_of_subscribers(subreddit):
+    """ test """
+    url = 'https://www.reddit.com/r/{}/about.json'.format(sys.argv[1])
+    headers = {
+        'User-Agent': 'My User Agent 1.0',
+        'From': 'youremail@domain.com'
+    }
+    x = requests.get(url, headers=headers, allow_redirects=False)
+    if x.status_code == 200:
+        return(json.loads(x.text)['data']['subscribers'])
+    else:
+        return(0)
