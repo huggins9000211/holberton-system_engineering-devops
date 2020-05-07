cbc2aed33e23f71704e8ba31e587c86d2d5791bc
Anthony <huggins9000211@gmail.com>
2020-05-07 15:42:40 -0400

    test

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -7,13 +7,16 @@ import sys
 
 def number_of_subscribers(subreddit):
     """ test """
-    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
+    url = 'https://www.reddit.com/r/{}/about.json'.format(sys.argv[1])
     headers = {
         'User-Agent': 'My User Agent 1.0',
         'From': 'youremail@domain.com'
     }
     x = requests.get(url, headers=headers, allow_redirects=False)
+    print(x.text)
     if x.status_code == 200:
         return(json.loads(x.text)['data']['subscribers'])
     else:
         return(0)
+
+number_of_subscribers('funny')
